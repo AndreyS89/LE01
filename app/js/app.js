9d63e5fd6a147e37fe3e52e558bfe08e765ac003
Andrey Samarin <samarinandrey55@gmail.com>
2020-09-22 19:10:15 +0300

    1 переменне

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	console.log(5);
+	// 1 Переменне
+	// camelCase
+	const firstName = "Andrey"
+	const lastName = "Samarin" // string
+	let age = 26 // number
+	const isProgrammer = true // boolean
+
+	const   = 'private'
+	const $ = 'some value'
+
+	// const if = 'mkef' // err
+	const withNum = '5'
+	// const 5withNum = 5 // err
 
 });
