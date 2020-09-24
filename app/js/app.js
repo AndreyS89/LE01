d6bbe2182e2b9bc4b597f65928acf13de2b74553
Andrey Samarin <samarinandrey55@gmail.com>
2020-09-24 15:15:30 +0300

    3 Типы данных

--- app/js/app.js
+++ app/js/app.js
@@ -2,23 +2,53 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// 1 Переменне
 	// camelCase
-	const firstName = "Andrey"
+	// const firstName = "Andrey"
 	// const lastName = "Samarin" // string
-	let age = 26 // number
-	const isProgrammer = true // boolean
+	// let age = 31 // number
+	// const isProgrammer = true // boolean
 
 	// const   = 'private'
-	const $ = 'some value'
+	// const $ = 'some value'
 
 	// const if = 'mkef' // err
-	const withNum = '5'
+	// const withNum = '5'
 
 	// ---------------------------------------------------------------------	
 	// 2 Мутирование
 	// console.log('Имя человека: ' + firstName + ', а возраст еловека: ' + age)
 	// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
 
-	const lastName = prompt('Введите фамилию')
-	console.log('Имя человека: ' + firstName + ', Фамилия: ' + lastName)
+	// const lastName = prompt('Введите фамилию')
+	// console.log('Имя человека: ' + firstName + ', Фамилия: ' + lastName)
+
+	// ---------------------------------------------------------------------	
+	// 3 Операторы
+	// let currentYear = 2020
+	// const birthYear = 1993
+
+	// const age = currentYear + birthYear
+
+	// const a = 10
+	// const b = 5
+
+	// let c = 32
+	// c = c + a
+// 	c += a
+// 	c -= a
+// 	c *= a
+// 	c /= a
+
+// 	console.log(a + b)
+// 	console.log(a - b)
+// 	console.log(a * b)
+// 	console.log(a / b)
+// 	console.log(currentYear++)
+// 	console.log(++currentYear)
+// 	console.log(--currentYear)
+// 	console.log(currentYear)
+// 	console.log(c)
+
+	// ---------------------------------------------------------------------	
+	// 4 Типы данных
 
 });
