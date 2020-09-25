d6fc859e3b9acf1b096e22755f8b73b7af9d93c6
Andrey Samarin <samarinandrey55@gmail.com>
2020-09-25 18:04:33 +0300

    4 Типы данных

--- app/js/app.js
+++ app/js/app.js
@@ -33,22 +33,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// let c = 32
 	// c = c + a
-// 	c += a
-// 	c -= a
-// 	c *= a
-// 	c /= a
+	// c += a
+	// c -= a
+	// c *= a
+	// c /= a
 
-// 	console.log(a + b)
-// 	console.log(a - b)
-// 	console.log(a * b)
-// 	console.log(a / b)
-// 	console.log(currentYear++)
-// 	console.log(++currentYear)
-// 	console.log(--currentYear)
-// 	console.log(currentYear)
-// 	console.log(c)
+	// console.log(a + b)
+	// console.log(a - b)
+	// console.log(a * b)
+	// console.log(a / b)
+	// console.log(currentYear++)
+	// console.log(++currentYear)
+	// console.log(--currentYear)
+	// console.log(currentYear)
+	// console.log(c)
 
 	// ---------------------------------------------------------------------	
 	// 4 Типы данных
+	const isProgrammer = true
+	const name = 'Andrey'
+	const age = '31'
+	let x 
+	console.log(typeof isProgrammer)
+	console.log(typeof name)
+	console.log(typeof age)
+	console.log(typeof x)
+	console.log(null)
 
+	// ---------------------------------------------------------------------	
+	// 5 Приоритет операторов
 });
