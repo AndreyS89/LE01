50fdc82f44996379b2ebe382409521b6163b382c
Andrey Samarin <samarinandrey55@gmail.com>
2020-09-30 10:52:00 +0300

    2 | Числа / 2

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
 	const num = 42        // Integer
 	const float = 42.42   // Float 
 	const pow = 10e3      // 10000
-	console.log(pow)
 
+	console.log('MAX_SAFE_INTEGER: ', Number.MAX_SAFE_INTEGER)
+	console.log('Math.pow 53:', Math.pow(2, 53) - 1)
+	console.log('MIN_SAFE_INTEGER: ', Number.MIN_SAFE_INTEGER)
+	console.log('MIN_VALUE: ', Number.MIN_VALUE)
+	console.log('POSITIVE_INFINITY: ', Number.POSITIVE_INFINITY)
+	console.log('NEGATIVE_INFINITY: ', Number.NEGATIVE_INFINITY)
+	console.log('2 / 0: ', 2 / 0)
+	console.log(Number.NaN) // Not A Number
+	console.log(typeof Nan)
+	const weird = 2 / undefined
+	console.log(Number.isNaN(weird))
+	console.log(Number.isNaN(42))
+	
 });
 
 
