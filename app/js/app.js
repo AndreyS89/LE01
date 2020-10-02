5dc6bd9a94ee68757dee6303b1c0d9c8c5c9815a
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-02 12:01:28 +0300

    3 | Строки / 1

--- app/js/app.js
+++ app/js/app.js
@@ -1,69 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// // Number 
-	// const num = 42        // Integer
-	// const float = 42.42   // Float 
-	// const pow = 10e3      // 10000
+	const name = 'Андрей'
+	const age = 31
 
-	// console.log('MAX_SAFE_INTEGER: ', Number.MAX_SAFE_INTEGER)
-	// console.log('Math.pow 53:', Math.pow(2, 53) - 1)
-	// console.log('MIN_SAFE_INTEGER: ', Number.MIN_SAFE_INTEGER)
-	// console.log('MIN_VALUE: ', Number.MIN_VALUE)
-	// console.log('POSITIVE_INFINITY: ', Number.POSITIVE_INFINITY)
-	// console.log('NEGATIVE_INFINITY: ', Number.NEGATIVE_INFINITY)
-	// console.log('2 / 0: ', 2 / 0)
-	// console.log(Number.NaN) // Not A Number
-	// console.log(typeof Nan)
-	// const weird = 2 / undefined
-	// console.log(Number.isNaN(weird))
-	// console.log(Number.isNaN(42))
-	// console.log(Number.isFinite(isFinite))
-	// console.log(Number.isFinite(42))
-
-	// const stringInt = '40'
-	// const stringFloat = '40.42'
-	// console.log(Number.parseInt(stringInt) + 2)
-	// console.log(Number(stringInt) + 2)
-	// console.log(+stringInt + 2)
-
-	// console.log(parseFloat(stringInt) + 2)
-	// console.log(+stringInt + 2)
-
-	// console.log(0.4 + 0.6) // 0.6
-	// console.log((0.4 + 0.6).toFixed(1)) // 0.6
-	// console.log(parseFloat(0.4 + 0.6).toFixed(1)) // 0.6	
-
-	// 2 BigInt
-	// console.log(909090909897979798789789n - 9090909098979797989n)
-	// console.log(909090909897979798789789n)
-	// console.log(typeof 909090909897979798789789.879789798n) // error	
-
-	// console.log(10n - 4) // error
-	// console.log(parseInt(10n) - 4)
-	// console.log(10n - BigInt(4))
-	// console.log(5n / 2n)
-	
-	// 3 Math 
-	// console.log(Math.E)
-	// console.log(Math.PI)
-
-	// console.log(Math.sqrt(25))
-	// console.log(Math.pow(5, 3))
-	// console.log(Math.abs(-42))
-	// console.log(Math.max(42, 23, 3, 11))
-	// console.log(Math.floor(4.9))
-	// console.log(Math.ceil(4.9))
-	// console.log(Math.round(4.9))
-	// console.log(Math.trunc(4.9))
-	// console.log(Math.random())
-
-	// 4 Example
-	function getRandomBetween(min, max) {
-		return Math.floor(Math.random() * (max - min) + min)
+	function getAge() {
+		return age
 	}
 
-	console.log(getRandomBetween(10, 42))
-	// 2.15:10
+	// const output = 'Привет, меня зовут: ' + name + ' и мой возраст: ' + age + ' год.'
+	// const output = `Привет меня зовут ${name} и мой возраст ${age < 20 ? 'A' : 'B'} год.`	
+
+	const output = `
+		<div>This is div</div'
+		<p>This is p</p>
+	`
+	console.log(output)
+	// 2.28:41
 });
 
 
