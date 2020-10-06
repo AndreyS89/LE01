31a49378bdae06ab76ef24a9a89d58a9964c0674
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-06 13:01:27 +0300

    5 | Массивы | 2

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // 1. массивы
+    // 1. Массивы
     const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес',]	
     const fib = [1, 1, 2, 3, 5, 8, 13]
+    // console.log(cars)
 
-    // 3:05:51
+    // Function
+    function addItemToEnd() {
+
+    }
+
+    //Method
+    cars.push('Рено')                // Добавляет в конец массива
+    cars.unshift('Волга')            // Добавляет в начало массива
+    const firstItem = cars.shift()   // Удаляет и возращает первый элемент из массива
+    const lastCar = cars.pop()       // Удаляет и возращает последний элемент из массива
+    console.log(firstItem)
+    console.log(cars)
+
+    // 3:14:15
 
 });
 
