a03016c4a62fb7b7dc2c4bf94a4404e8cf2087f2
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-12 15:05:36 +0300

    5 | Массивы | 3

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 1. Массивы
     const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес',]	
+
+    const people = [
+        {name: 'Andrey', budget: 4200},
+        {name: 'Elena', budget: 3500},
+        {name: 'Victoria', budget: 1700}
+    ]
+
     const fib = [1, 1, 2, 3, 5, 8, 13]
     // console.log(cars)
 
@@ -20,12 +27,34 @@ document.addEventListener("DOMContentLoaded", function() {
     // console.log(cars.reverse())
     // console.log(cars)
 
+    // const index = cars.indexOf('БМВ')    
+    // cars[index] = 'Porsche'
+    // console.log(cars)
+
+    // let findedPerson
+    // for (const person of people) {
+    //     if (person.budget === 3500) {
+    //         findedPerson = person
+    //     }
+    // }
+
+    // console.log(findedPerson)
+
+    // const index = people.findIndex(function(person) {
+    //     return person.budget === 3500
+    // })
+
+    const person = people.find(function(person) {
+        return person.budget === 3500
+    })
+    console.log(person)
+
     // Задача 1
     // const text = 'Привет, мы изучаем JavaScript'
     // const reverseText = text.split('').reverse().join('')
     // console.log(reverseText)
 
-    // 3:20:00
+    // 3:27:00
 
 });
 
