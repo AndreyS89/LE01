684669fe0ed90eb6ea711333ca9f73e57db8fa95
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-16 18:21:23 +0300

    6 | Объекты | 1

--- app/js/app.js
+++ app/js/app.js
@@ -1,101 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // 1. Массивы
-    const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес',]	
+    const person = {
+        name: 'Andrey', 
+        age: 31,
+        isProgrammer: true,
+        languages: ['ru', 'en'],
+        '': '',
+        greet() {
+            console.log('greet from person')
+        }
+    }
 
-    // const people = [
-    //     {name: 'Andrey', budget: 4200},
-    //     {name: 'Elena', budget: 3500},
-    //     {name: 'Victoria', budget: 1700}
-    // ]
-
-    const fib = [1, 1, 2, 3, 5, 8, 13]
-    // console.log(cars)
-
-    // Function
-    // function addItemToEnd() {
-    // }
-
-    //Method
-    // cars.push('Рено')                // Добавляет в конец массива
-    // cars.unshift('Волга')            // Добавляет в начало массива
-    // const firstItem = cars.shift()   // Удаляет и возращает первый элемент из массива
-    // const lastCar = cars.pop()       // Удаляет и возращает последний элемент из массива
-    // console.log(firstItem)
-    // console.log(cars)
-
-    // console.log(cars.reverse())
-    // console.log(cars)
-
-    // const index = cars.indexOf('БМВ')    
-    // cars[index] = 'Porsche'
-    // console.log(cars)
-
-    // let findedPerson
-    // for (const person of people) {
-    //     if (person.budget === 3500) {
-    //         findedPerson = person
-    //     }
-    // }
-
-    // console.log(findedPerson)
-
-    // const index = people.findIndex(function(person) {
-    //     return person.budget === 3500
-    // })
-
-    // const person = people.find(function(person) {
-    //     return person.budget === 3500
-    // })
-
-    // const person = people.find(person => person.budget === 3500)
-    // console.log(person)
-
-    // console.log(cars.includes('Мазда'))
-
-    // const upperCaseCars = cars.map(car => {
-    //     return car.toUpperCase()
-    // })
-    // console.log(upperCaseCars)
-    // console.log(cars)
-
-    // const pow2 = num => num ** 2
-    // const sqrt = num => Math.sqrt(num)
-
-    // const pow2Fib = fib.map(pow2).map(Math.sqrt)
-
-    // console.log(fib)
-    // console.log(pow2Fib)
-    
-    // const pow2 = num => num ** 2
-    // const pow2Fib = fib.map(pow2)
-    // const filtersNumbers = pow2Fib.filter(num => num > 20)
-
-    // console.log(pow2Fib)
-    // console.log(filtersNumbers)
-
-    // Задача 1
-    // const text = 'Привет, мы изучаем JavaScript'
-    // const reverseText = text.split('').reverse().join('')
-    // console.log(reverseText)
-
-
-    // const people = [
-    //     {name: 'Andrey', budget: 4200},
-    //     {name: 'Elena', budget: 3500},
-    //     {name: 'Victoria', budget: 1700}
-    // ]
-
-    // const allBudget = people
-    //     .filter(person => person.budget > 2000)
-    //     .reduce((acc, person) => {
-    //         acc += person.budget     
-    //         return acc
-    // }, 0)
-
-    // console.log(allBudget)
-
-    const displayItems = allItems.filter(intem => displayItems.name.indexOf('phone') != _1)
+    console.log(person.name)
+    console.log(person['age'])
 
     // 3:34:52
 
