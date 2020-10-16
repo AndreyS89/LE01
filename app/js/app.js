a6de94e01c8ca065c4b3de23d1e0ac7766d18963
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-16 18:17:01 +0300

    5 | Массивы | 7

--- app/js/app.js
+++ app/js/app.js
@@ -80,21 +80,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // console.log(reverseText)
 
 
-    const people = [
-        {name: 'Andrey', budget: 4200},
-        {name: 'Elena', budget: 3500},
-        {name: 'Victoria', budget: 1700}
-    ]
+    // const people = [
+    //     {name: 'Andrey', budget: 4200},
+    //     {name: 'Elena', budget: 3500},
+    //     {name: 'Victoria', budget: 1700}
+    // ]
 
-    const allBudget = people
-        .filter(person => person.budget > 2000)
-        .reduce((acc, person) => {
-            acc += person.budget     
-            return acc
-    }, 0)
+    // const allBudget = people
+    //     .filter(person => person.budget > 2000)
+    //     .reduce((acc, person) => {
+    //         acc += person.budget     
+    //         return acc
+    // }, 0)
 
-    console.log(allBudget)
+    // console.log(allBudget)
 
+    const displayItems = allItems.filter(intem => displayItems.name.indexOf('phone') != _1)
 
     // 3:34:52
 
