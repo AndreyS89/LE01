4e1e07b069a3a3ec55a98d99d430e3e5784c4a9d
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-18 20:42:57 +0300

    6 |  Объекты | 3

--- app/js/app.js
+++ app/js/app.js
@@ -2,19 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const person = {
         name: 'Andrey', 
-        age: 31,
+        age: undefined,
         isProgrammer: true,
         languages: ['ru', 'en'],
-        '': '',
+        'complex key': 'Complex Value',
+        ['key_' + (1 + 3)]: 'Computed Key', // key_4
         greet() {
             console.log('greet from person')
         }
     }
 
-    console.log(person.name)
-    console.log(person['age'])
+    // console.log(person.name)
+    // const ageKey = 'age'
+    // console.log(person[ageKey])
+    // console.log(person['complex key'])
+    // person.greet()
 
-    // 3:48:23
+    // person.age++
+    // person.languages.push('by')
+    // person['key_4'] = undefined
+    // delete person['key_4']
+
+    // console.log(person)
+    // console.log(person['key_4'])
+
+    // const name = person.name
+
+    const {name, age: personAge = 10, languages} = person
+
+    console.log(name, personAge, languages)
+
+
+    // 3:56:35
 
 });
 
