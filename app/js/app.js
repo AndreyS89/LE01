deeaf5db4924f82cd23940e751a1defbd703510a
Andrey Samarin <samarinandrey55@gmail.com>
2020-10-25 18:54:19 +0300

    6 |  Объекты | 4

--- app/js/app.js
+++ app/js/app.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
         age: undefined,
         isProgrammer: true,
         languages: ['ru', 'en'],
-        'complex key': 'Complex Value',
-        ['key_' + (1 + 3)]: 'Computed Key', // key_4
+        // 'complex key': 'Complex Value',
+        // ['key_' + (1 + 3)]: 'Computed Key', // key_4
         greet() {
             console.log('greet from person')
         }
@@ -28,12 +28,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // const name = person.name
 
-    const {name, age: personAge = 10, languages} = person
+    // const {name, age: personAge = 10, languages} = person
 
-    console.log(name, personAge, languages)
+    // console.log(name, personAge, languages)
+
+    // console.log(person)
+
+    // for (let key in person) {
+    //     if (person.hasOwnProperty(key)){
+            // console.log('key: ', key)
+            // console.log('value: ', person[key])
+    //     }        
+    // }
+
+    const keys = Object.keys(person)
+    // console.log(keys)
+
+    // const values = Object.values(person)
+    // console.log(values)
+
+    keys.forEach((key) => {
+        console.log('key: ', key)
+        console.log('value: ', person[key])
+    })
 
 
-    // 3:56:35
+    // 4:03:07
 
 });
 
