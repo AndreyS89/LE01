8042a20cdbef9d7b31e1aab5177c5fda60c2b02b
Andrey Samarin <samarinandrey55@gmail.com>
2020-12-19 13:05:36 +0300

    8 | Работа с DOM | 4

--- app/js/app.js
+++ app/js/app.js
@@ -4,11 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // alert('1')
 
     const heading = document.getElementById('hello')
+    // const heading2 = document.getElementsByTagName('h2')[0]
+    // const heading2 = document.getElementsByClassName('h2-class')[0]
+    // console.log(heading2)
+    
+    // console.dir(heading.id)
+    // console.dir(heading.textContent)
 
-    console.dir(heading.id)
-    console.dir(heading.textContent)
+    setTimeout(() => {
+        addStylelesTo(heading)
+    }, 1500)
 
-    heading.textContent = 'Changed from JavaScript'
+    function addStylelesTo(naode) {
+        heading.textContent = 'Changed from JavaScript'
+        heading.style.color = 'red'
+        heading.style.textAlign = 'center'
+        heading.style.backgroundColor = 'black'
+        heading.style.padding = '2rem'
+    }
     
     // 4:59:19
 
