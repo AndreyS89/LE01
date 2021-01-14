dd43e3b0a6b17248c0927926b39885decb10466c
Andrey Samarin <samarinandrey55@gmail.com>
2021-01-14 08:40:34 +0300

    1_Prototype_1

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    // Урок 1. JavaScript. Что такое prototype. JavaScript Prototype (English Subs)
+    // -------------------------------------- //
 
+    // const person = {
+        // name: 'Andrey',
+        // age: 31,
+        // greet: function() {
+        //     console.log('Greet!')
+        // }
+        // }
+        
+    const person = new Object({
+        name: 'Andrey',
+        age: 31,
+        greet: function() {
+            console.log('Greet!')
+        }        
+    })
 
+    Object.prototype.sayHello = function() {
+        console.log('Hello!')
+    }
+
+    const lena = Object.create(person)
+    lena.name = 'Elena'
+        
 });
