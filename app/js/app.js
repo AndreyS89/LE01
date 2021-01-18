03e1e5f4c2dac38c07556b5cc44032b38000f81c
Andrey Samarin <samarinandrey55@gmail.com>
2021-01-18 14:23:18 +0300

    02_this_bind_call_apply_2

--- app/js/app.js
+++ app/js/app.js
@@ -6,15 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
     function hello() {
         console.log('Hello', this)
     }
-    
+
     const person = {
         name: 'Andrey',
         age: 31,
         sayHello: hello,
         sayHelloWindow: hello.bind(window),
-        logInfo: function() {
+        logInfo: function(job, phone) {
+            console.log(`${this.name} info:`)
             console.log(`Name is ${this.name}`)
             console.log(`Age is ${this.age}`)
+            console.log(`Job is ${job}`)
+            console.log(`Phone is ${phone}`)
+            console.groupEnd()
         }
     }
 
@@ -23,7 +27,24 @@ document.addEventListener("DOMContentLoaded", function() {
         age: 23
     }
 
-    person.logInfo.bind(lena)()
+    // const fnLenaInfoLof = person.logInfo.bind(lena)()
+    // fnLenaInfoLof('Frontend', '8-999-123-12-23')
+
+    // person.logInfo.bind(lena, 'Frontend', '8-999-123-12-23')
+    // person.logInfo.call(lena, 'Frontend', '8-999-123-12-23')
+    // person.logInfo.apply(lena, ['Frontend', '8-999-123-12-23'])
+
+    const array = [1, 2, 3, 4, 5]
+
+    // function multBy(arr, n) {
+    //     return arr.map(function(i) {
+    //         return i * n
+    //     })
+    // }
+
+    Array
+
+    console.log(multBy(array, 15))
 
     // -------------------------------------- //
     // TIME | 9:33
