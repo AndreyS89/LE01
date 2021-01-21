0e23bb9d96a415e93c65fcb922b9bedac37d472b
Andrey Samarin <samarinandrey55@gmail.com>
2021-01-21 15:53:09 +0300

    04_Assynch_Event_Loop_JS_SetTimeout0_2

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // Урок 4. JavaScript. Асинхронность.Что такое Event Loop. JS SetTimeout 0
+    // https://youtu.be/vIZs5tH-HGQ
     // -------------------------------------- //
-
     
+    console.log('Start')
+
+    console.log('Start 2')
+
+    function timeout2sec() {
+        console.log('timeout2sec')
+    }
+
+    window.setTimeout(function() {
+        console.log('Inside timeout, after 2 second')
+    }, 200)
+
+    setTimeout(timeout2sec, 2000)
+
+    console.log('End')
 
     // -------------------------------------- //
-    // TIME | 00:00
+    // TIME | 05:36
         
 });
